b641dd3ce3581c311e9096bd34dfa910386b86d1
wlstn52 <jskim0285@gmail.com>
2026-01-27 15:58:24 +0900

    shooting system 제작

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootingSystem.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class ShootingSystem implements Task{
+    private DcMotor shootingMotor;
+    private Servo liftingServo;
+    private ElapsedTime runtime;
+    private double shooting_time;
+    enum Status{
+            IDLE,
+        SHOOTING,
+    }
+    Status status = Status.IDLE;
+    static final double DEFAULT_LIFTING_ANGLE = 10;
+    static final double DELAY = 1.5;
+
+    ShootingSystem(DcMotor shootingMotor, Servo liftingServo, ElapsedTime timer){
+        this.shootingMotor = shootingMotor;
+        this.liftingServo = liftingServo;
+        runtime = timer;
+    }
+    public void init(){
+        shootingMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shootingMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        liftingServo.setDirection(Servo.Direction.FORWARD);
+    }
+    // 발사 모터를 가동
+    public void motorStart(double power) {
+        shootingMotor.setPower(power);
+    }
+    public void motorStop(){
+        shootingMotor.setPower(0);
+    }
+    // 발사 코드
+    public void shoot(){
+        if(status != Status.IDLE) return;
+        liftingServo.setPosition(DEFAULT_LIFTING_ANGLE / 300 );
+
+        status = Status.SHOOTING;
+        shooting_time = runtime.seconds();
+    }
+    // 상태 업데이트
+    public void process(){
+        switch (status){
+            case SHOOTING:
+                if(shooting_time + DELAY <= runtime.seconds()){
+                    liftingServo.setPosition(0);
+                    status = Status.IDLE;
+                }
+                break;
+        }
+    }
+    public boolean isBusy(){
+        return status != Status.IDLE;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testShooting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testShooting.java
@@ -10,31 +10,24 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Shooting Test", group="Test")
 public class testShooting extends LinearOpMode {
-    private DcMotor testMoter = null;
+    private DcMotor testMotor = null;
     private Servo liftingServo;
+    private ShootingSystem shootingSystem;
     private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode(){
-        testMoter = hardwareMap.get(DcMotor.class, "shooting_motor");
+        testMotor = hardwareMap.get(DcMotor.class, "shooting_motor");
         liftingServo = hardwareMap.get(Servo.class, "lifting_servo");
-        testMoter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        testMoter.setDirection(DcMotor.Direction.FORWARD);
-        liftingServo.setDirection(Servo.Direction.FORWARD);
-        liftingServo.setPosition(0);
-
-        telemetry.addData("Status", "ready to test");
-        telemetry.update();
-
+        runtime.reset();
+        shootingSystem = new ShootingSystem(testMotor, liftingServo, runtime);
+        shootingSystem.init();
         waitForStart();
-        liftingServo.setPosition(0.1);
-        while(opModeIsActive() && (runtime.seconds() < 5.0)){
-            telemetry.addData("Time", "%4.1f s Elapsed", runtime.seconds());
-            telemetry.update();
-        }
-        testMoter.setPower(0);
-        telemetry.addData("Status", "Complete");
-        telemetry.update();
+        shootingSystem.motorStart(0.8);
+        sleep(2000);
+        shootingSystem.shoot();
+        sleep(2000);
+        shootingSystem.motorStop();
         sleep(1000);
     }
 }
